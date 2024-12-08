29f81cabea60aa809011b349afc146e4a5c00829
Defoltini <nesti-1998@mail.ru>
2024-12-08 22:45:32 +0300

    Added test

--- src/test/java/CheckTextOnGithubTest.java
+++ src/test/java/CheckTextOnGithubTest.java
@@ -1,14 +1,19 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CheckTextOnGithubTest {
 
 
     @Test
-    void checkTextOnPage() {
-
+    void checkTextOnPageEnterprises() {
+        open("https://github.com/home");
+        $(byTagAndText("button","Solutions")).hover();
+        $(byTagAndText("a","Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(Condition.text("The AI-powered\n" +
+                "developer platform"));
+        sleep(5000);
     }
 }

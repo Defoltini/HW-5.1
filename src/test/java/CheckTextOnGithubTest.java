b3431fd9991f677a157120b1aa7326a6237e9f61
Defoltini <nesti-1998@mail.ru>
2024-12-08 22:23:07 +0300

    Added files

---
+++ src/test/java/CheckTextOnGithubTest.java
@@ -0,0 +1,14 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckTextOnGithubTest {
+
+
+    @Test
+    void checkTextOnPage() {
+
+    }
+}

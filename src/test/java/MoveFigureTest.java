e55316c759c8ddf1516988d847e2c0cee446255e
Defoltini <nesti-1998@mail.ru>
2024-12-08 23:13:29 +0300

    Added test with actions

--- src/test/java/MoveFigureTest.java
+++ src/test/java/MoveFigureTest.java
@@ -1,13 +1,22 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MoveFigureTest {
 
     @Test
     void MoveFigureOnPage() {
-        open("https://github.com/home");
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement one = $("#column-a").shouldBe(Condition.visible);
+        SelenideElement two = $("#column-b").shouldBe(Condition.visible);
+        actions()
+                .clickAndHold(one)
+                .moveToElement(two)
+                .release()
+                .build()
+                .perform();
+        $("#column-b").shouldHave(Condition.text("A"));
         sleep(5000);
     }
 }
